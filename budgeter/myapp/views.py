51c1f618a300f9858cfc7515ce886b299b623233
David Davini <dndavini3.14@gmail.com>
2024-11-08 00:26:41 -0800

    barebones business list functionality

--- budgeter/myapp/views.py
+++ budgeter/myapp/views.py
@@ -1,6 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .externalapis.overpass import nearby_stores 
+
 @api_view(['GET'])
 def sample_api(request):
-    return Response({"message": "Hello from Django!"})
+    radius = request.GET.get('radius')
+    lat = request.GET.get('lat')
+    lon = request.GET.get('lon')
+    print(lat,lon)
+    nodes = nearby_stores(
+        radius=radius,
+        lat=lat,
+        lon=lon,
+    )
+    return Response({"results": nodes})
+
