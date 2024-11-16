2d31cba750906a8522f42685dcfa8f705e552bcb
natecarman <125101921+natecarman@users.noreply.github.com>
2024-11-16 15:03:23 -0800

    Created Basic Login Page
    Created a temporary skeleton for a login page

---
+++ frontend/src/Login.js
@@ -0,0 +1,16 @@
+import {Input} from 'antd'
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Welcome To Clever Cash</h1>
+            <input type="text" placeholder="Username" />
+            <br />
+            <input type="password" placeholder="Password" />
+            <br />
+            <button>Login</button>
+        </div>
+    );
+}
+
+export default Login;
