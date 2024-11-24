2a7fbe2d7e3a0ed4d6f5cc94b73e1e800c72ba4a
eayala720 <eayala720@gmail.com>
2024-11-24 04:28:50 -0800

    fixed compilation issues with Ant Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import LogSpending from './LogSpending';
+import Login from './Login';
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
                         <Route path="/" element={<Home />} />
                         <Route path="/shops" element={<LocationSearch />} />
                         <Route path="/log" element={<LogSpending />} />
+                        <Route path="/login" element={<Login />} />
                     </Routes>
                 </div>
             </Router>
