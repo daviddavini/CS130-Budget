1ced3471cbe1052aa85d0a75a50c35d2269c4999
David Davini <dndavini3.14@gmail.com>
2024-11-30 23:41:04 -0800

    fix index.html path

--- budgeter/mysite/settings.py
+++ budgeter/mysite/settings.py
@@ -80,7 +80,7 @@ REST_FRAMEWORK = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'build')],
+        'DIRS': [os.path.join(BASE_DIR, '..', 'frontend', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
