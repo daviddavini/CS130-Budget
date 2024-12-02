d8115d092d929bc1be432beb6fc96c52ba5da4f1
David Davini <dndavini3.14@gmail.com>
2024-12-01 18:39:36 -0800

    fix django urls

--- budgeter/mysite/urls.py
+++ budgeter/mysite/urls.py
@@ -4,7 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("api/", include("myapp.urls")),
-    path('', TemplateView.as_view(template_name='index.html')),
-    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
     path("admin/", admin.site.urls),
+    re_path(r'^(?!admin/|api/).*$', TemplateView.as_view(template_name='index.html')),
 ]
